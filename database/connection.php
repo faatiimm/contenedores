8195839af588bc19f5ef273e0bbb57c9355d07eb
Fatima <fmakhloufimourdi@email.com>
2026-01-14 17:16:42 +0100

    archivos_config

--- database/connection.php
+++ database/connection.php
@@ -6,27 +6,34 @@
  */
 
 // Parámetros de configuración del servidor de base de datos
-$host = "127.0.0.1";        // Dirección IP del servidor local (Loopback)
-$user = "root";             // Usuario administrador por defecto en XAMPP
-$pass = "";                 // Contraseña (vacía por defecto en entornos de desarrollo local)
-$db   = "retrogaming_hub";  // Nombre de la base de datos que contiene las tablas del foro
-$port = 3306;               // Puerto estándar de comunicación para el servicio MySQL
+$host = getenv("DB_HOST");        // Dirección IP del servidor local (Loopback)
+$user = getenv("DB_USER");             // Usuario administrador por defecto en XAMPP
+$pass = getenv("DB_PASS");                 // Contraseña (vacía por defecto en entornos de desarrollo local)
+$db   = getenv("DB_NAME");  // Nombre de la base de datos que contiene las tablas del foro
+$port = 3307;               // Puerto estándar de comunicación para el servicio MySQL
 
 /**
  * Establecimiento de la conexión mediante la extensión MySQLi.
  * Se incluyen los 5 parámetros necesarios para garantizar la precisión de la ruta.
  */
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
-
-/**
- * Control de errores de conexión.
- * Si la conexión falla, se detiene la ejecución del script y se muestra el motivo técnico.
- * Es vital para el diagnóstico durante el desarrollo (Debug).
- */
-if (!$conn) {
-    die("Fallo de conexión: " . mysqli_connect_error());
-}
-
-// Opcional: Configuración del conjunto de caracteres a UTF-8 para evitar errores con tildes y eñes
-mysqli_set_charset($conn, "utf8mb4");
+ $conn = new mysqli($host, $user, $pass,$db);
+ $users = array();
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ } else {
+     $sql = 'SELECT * FROM users';
+     
+     if ($result = $conn->query($sql)) {
+         while ($data = $result->fetch_object()) {
+             $users[] = $data;
+         }
+     }
+     
+     foreach ($users as $user) {
+        echo "<br>";
+        echo $user->username . " " . $user->password;
+        echo "<br>";
+    }
+ }
+ mysqli_close($conn);
 ?>
