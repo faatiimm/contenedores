7444f5505f4dff992befda452de682a5089e3241
Fatima <fmakhloufimourdi@email.com>
2026-02-11 17:04:25 +0100

    pruebas

---
+++ src/Utils.php
@@ -0,0 +1,19 @@
+<?php
+
+class Utils
+{
+    public static function sumar($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public static function esEmailValido($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public static function esMayorDeEdad($edad)
+    {
+        return $edad >= 18;
+    }
+}
