41834adc8ac2c07d7f374c7300c9d8eeee9f463d
faatiimm <fmakhloufimourdi@gmail.com>
2026-02-11 16:05:01 +0000

    Deploying to gh-pages from @ faatiimm/contenedores@7444f5505f4dff992befda452de682a5089e3241 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "$_abcdefilnprstuw",
+  1: "acdfilnprtu",
+  2: "$_abcefistw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Archivos",
+  2: "Variables"
+};
+
