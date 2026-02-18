8b86bfbd7430a79d607487c7ba05b2327e2a9645
Fatima <fmakhloufimourdi@email.com>
2026-02-18 16:34:38 +0100

    mala contraseña

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
 $host = getenv("DB_HOST") ?: "db";
 $user = getenv("DB_USER") ?: "userphp";
-$pass = getenv("DB_PASS") ?: "badpasswd";
+$pass = "badpasswd";
 $db   = getenv("DB_NAME");
 $port = getenv("DB_PORT");
 
